f23604a5d5e94959e490e1409f096dfbe8f60ff4
FranciscoValerio98 <U22210871@utp.edu.pe>
2026-01-22 01:32:01 -0500

    CRUD profiles (incomplete)

---
+++ src/services/administrador.service.js
@@ -0,0 +1,61 @@
+const prisma = require("../../prisma/client")
+
+exports.getAllAdmin = async () => {
+    return prisma.administrador.findMany();
+};
+
+exports.getAdmin = async (id) => {
+    return prisma.administrador.findUnique({
+        where: { usuario_id: Number(id) }
+    });
+};
+
+exports.createAdmin = async ({ userData, adminData }) => {
+    const user = await prisma.usuarios.create({
+        data: {
+            ...userData
+        }
+    });
+
+    const admin = await prisma.administrador.create({
+        data: {
+            usuario_id: user.id,
+            cargo: adminData.cargo,
+            area: adminData.area,
+            sede_id: adminData.sede_id,
+        },
+        include: { usuarios: true, sedes: true }
+    });
+
+    return admin;
+};
+
+exports.updateAdmin = async (id, data) => {
+    return prisma.usuarios.update({
+        where: { id: Number(id) },
+        data: {
+            nombres: data.nombres,
+            apellidos: data.apellidos,
+            email: data.email,
+            telefono_personal: data.telefono_personal,
+            fecha_nacimiento: data.fecha_nacimiento,
+            administrador: {
+                update: {
+                    sede_id: data.sede_id,
+                    cargo: data.cargo,
+                    area: data.area
+                }
+            }
+        },
+        include: {
+            administrador: true
+        }
+    })
+}
+
+exports.deleteAdmin = async (id) => {
+    return prisma.$transaction([
+        prisma.administrador.deleteMany({ where: { usuario_id: Number(id) } }),
+        prisma.usuarios.delete({ where: { id: Number(id) } })
+    ]);
+}
