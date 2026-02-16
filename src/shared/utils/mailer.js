5ea87360e89af37f88592eedda7cff4959470730
YazminSuybate <yaaazscx@gmail.com>
2026-02-15 19:52:29 -0500

    Correccion de mailer

--- src/shared/utils/mailer.js
+++ src/shared/utils/mailer.js
@@ -6,12 +6,16 @@ dotenv.config();
 const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
-  secure: false, 
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 10000,
+  greetingTimeout: 5000,
+  socketTimeout: 15000,
   tls: {
+    ciphers: 'SSLv3',
     rejectUnauthorized: false
   }
 });
