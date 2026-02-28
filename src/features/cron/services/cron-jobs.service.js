e047157eb68a4a2a16e27faababedb0afd8af4b2
EduardoVargasZumaeta <u22234544@utp.edu.pe>
2026-02-28 13:22:46 -0500

    feat: Implement cron job scheduling for automated tasks and a Twilio WhatsApp messaging service.

--- src/features/cron/services/cron-jobs.service.js
+++ src/features/cron/services/cron-jobs.service.js
@@ -97,7 +97,7 @@ export const iniciarCronJobs = () => {
   // ------------------------------------------------------------------
 
   cron.schedule(
-    '* * * * *',
+    '* 8 * * *',
     async () => {
       logger.info(`[CRON] Buscando cumpleañeros de hoy: ${new Date().toISOString()}`);
       try {
