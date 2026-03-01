70d9f2b233bdad53b9fda68b721d53b0ad544153
EduardoVargasZumaeta <u22234544@utp.edu.pe>
2026-02-28 22:46:50 -0500

    g

--- src/features/cron/services/cron-jobs.service.js
+++ src/features/cron/services/cron-jobs.service.js
@@ -97,7 +97,7 @@ export const iniciarCronJobs = () => {
   // ------------------------------------------------------------------
 
   cron.schedule(
-    '45 22 * * *',
+    '48 22 * * *',
     async () => {
       logger.info(`[CRON] Buscando cumpleañeros de hoy: ${new Date().toISOString()}`);
       try {
